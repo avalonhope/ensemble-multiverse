256bb8706b4866857141353cb8e8701d35671816
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-01-27 14:28:53 +0000

    Create countries.py

---
+++ typeclasses/countries.py
@@ -0,0 +1,19 @@
+"""
+Country
+Countries are simple containers located within a Planet.
+"""
+
+from evennia import DefaultRoom
+
+
+class Country(DefaultRoom):
+    """
+    Countries are like any Object, except their location 
+    is within a Planet. They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+    See examples/object.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    pass
