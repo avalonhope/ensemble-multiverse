1cb984ca0e51623fd34964f8a37b77fe3219acfa
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-04 14:41:57 +0000

    Update characters.py

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -42,19 +42,4 @@ class Character(DefaultCharacter):
         self.db.speed = 1
         self.db.health = 1
         
-   # the result is always non-negative
-    @deal.post(lambda result: result >= 1.0)
-    # the function has no side-effects
-    @deal.pure
-    def proficiency (experience):
-        """
-        Calculate a charcater's skill proficency based on experience points.
-        
-        The result is 1 plus one tenth of the cube root of the experience points.
-        
-        For example: 1000 experience points gives a proficency of 2.0.
-        """
-        if experience <= 0:
-            return 1.0
-        return 1.0 + (0.1 * (round((experience ** 0.3), 2))
-        
+  
