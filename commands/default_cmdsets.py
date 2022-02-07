21c882537fed3ce86eb90b58305662777eae85bd
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-07 14:16:16 +0000

    Update default_cmdsets.py

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -35,6 +35,9 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         #
         # any commands you add below will overload the default ones.
         #
+        self.add(command.CmdCreateNPC())
+        self.add(command.CmdEditNPC())
+        self.add(command.CmdNPC())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
