7deffefc300c3baf0d0fbbdac3c85f2f6e633750
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-08 14:31:16 +0000

    Update command.py

--- commands/command.py
+++ commands/command.py
@@ -268,148 +268,44 @@ class CmdTrainSkill(Command):
         return
 
     
-class CmdCreateNPC(Command):
+class CmdRecruitCompanion(Command):
     """
-    create a new npc
+    create a new companion and add to party
 
     Usage:
-        +createNPC <name>
+        +createCompanion <name>
 
-    Creates a new, named NPC. The NPC will start with a Power of 1.
+    Creates a new, named companion.
     """
-    key = "+createnpc"
-    aliases = ["+createNPC"]
-    locks = "call:not perm(nonpcs)"
-    help_category = "mush"
+    key = "+receruitcompanion"
+    aliases = ["+recruitCompanion"]
+    locks = "call:not perm(nocompanions)"
+    help_category = "party"
     
     def func(self):
         "creates the object and names it"
         caller = self.caller
         if not self.args:
-            caller.msg("Usage: +createNPC <name>")
+            caller.msg("Usage: +createCompanion <name>")
             return
         if not caller.location:
-            # may not create npc when OOC
-            caller.msg("You must have a location to create an npc.")
+            # may not create companion when OOC
+            caller.msg("You must have a location to recruit a companion.")
             return
         # make each part of name always start with capital letter
         name = self.args.strip().title()
-        # create npc in caller's location
+        # create companion in caller's location
         npc = create_object("characters.Character",
                       key=name,
                       location=caller.location,
                       locks="edit:id(%i) and perm(Builders);call:false()" % caller.id)
+        # add to party
+        if caller.db.party is None:
+            caller.db.party = [name]
+        else:
+            caller.db.party.append(name)
         # announce
-        message = "%s created the NPC '%s'."
+        message = "%s recruited '%s'."
         caller.msg(message % ("You", name))
         caller.location.msg_contents(message % (caller.key, name),
                                                 exclude=caller)
-
-
-class CmdEditNPC(Command):
-    """
-    edit an existing NPC
-
-    Usage:
-      +editnpc <name>[/<attribute> [= value]]
-
-    Examples:
-      +editnpc mynpc/power = 5
-      +editnpc mynpc/power    - displays power value
-      +editnpc mynpc          - shows all editable
-                                attributes and values
-
-    This command edits an existing NPC. You must have
-    permission to edit the NPC to use this.
-    """
-    key = "+editnpc"
-    aliases = ["+editNPC"]
-    locks = "cmd:not perm(nonpcs)"
-    help_category = "mush"
-
-    def parse(self):
-        "We need to do some parsing here"
-        args = self.args
-        propname, propval = None, None
-        if "=" in args:
-            args, propval = [part.strip() for part in args.rsplit("=", 1)]
-        if "/" in args:
-            args, propname = [part.strip() for part in args.rsplit("/", 1)]
-        # store, so we can access it below in func()
-        self.name = args
-        self.propname = propname
-        # a propval without a propname is meaningless
-        self.propval = propval if propname else None
-
-    def func(self):
-        "do the editing"
-
-        allowed_propnames = ("power", "attribute1", "attribute2")
-
-        caller = self.caller
-        if not self.args or not self.name:
-            caller.msg("Usage: +editnpc name[/propname][=propval]")
-            return
-        npc = caller.search(self.name)
-        if not npc:
-            return
-        if not npc.access(caller, "edit"):
-            caller.msg("You cannot change this NPC.")
-            return
-        if not self.propname:
-            # this means we just list the values
-            output = f"Properties of {npc.key}:"
-            for propname in allowed_propnames:
-                output += f"\n {propname} = {npc.attributes.get(propname, default='N/A')}"
-            caller.msg(output)
-        elif self.propname not in allowed_propnames:
-            caller.msg(f"You may only change {', '.join(allowed_propnames)}.")
-        elif self.propval:
-            # assigning a new propvalue
-            # in this example, the properties are all integers...
-            intpropval = int(self.propval)
-            npc.attributes.add(self.propname, intpropval)
-            caller.msg(f"Set {npc.key}'s property {self.propname} to {self.propval}")
-        else:
-            # propname set, but not propval - show current value
-            caller.msg(f"{npc.key} has property {self.propname} = {npc.attributes.get(self.propname, default='N/A')}")
-
-    
-class CmdNPC(Command):
-    """
-    controls an NPC
-
-    Usage:
-        +npc <name> = <command>
-
-    This causes the npc to perform a command as itself. It will do so
-    with its own permissions and accesses.
-    """
-    key = "+npc"
-    locks = "call:not perm(nonpcs)"
-    help_category = "mush"
-
-    def parse(self):
-        "Simple split of the = sign"
-        name, cmdname = None, None
-        if "=" in self.args:
-            name, cmdname = self.args.rsplit("=", 1)
-            name = name.strip()
-            cmdname = cmdname.strip()
-        self.name, self.cmdname = name, cmdname
-
-    def func(self):
-        "Run the command"
-        caller = self.caller
-        if not self.cmdname:
-            caller.msg("Usage: +npc <name> = <command>")
-            return
-        npc = caller.search(self.name)
-        if not npc:
-            return
-        if not npc.access(caller, "edit"):
-            caller.msg("You may not order this NPC to do anything.")
-            return
-        # send the command order
-        npc.execute_cmd(self.cmdname)
-        caller.msg(f"You told {npc.key} to do '{self.cmdname}'.")
