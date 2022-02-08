020ce2ae84c3a01412c24cd829f51b465a22250c
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-08 14:43:25 +0000

    Update default_cmdsets.py

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -36,7 +36,8 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         # any commands you add below will overload the default ones.
         #
         self.add(command.CmdRecruitCompanion())
-
+        self.add(command.CmdMeditate())
+        
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
     """
