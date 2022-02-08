524178581085ab9ec37341d4153911f08d4e29d7
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-08 14:49:24 +0000

    Create innerworld.py

---
+++ typeclasses/innerworld.py
@@ -0,0 +1,13 @@
+"""
+Inner Worlds
+Inner Worlds exist within characters that are Plural Systems.
+"""
+from evennia import DefaultRoom
+
+
+class Home(DefaultRoom):
+    """
+    Home location for arriving at an inner world.
+    """
+
+    pass
