909eb5e01ce0f993a96d3a4f3780f30a856d259d
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-14 11:45:38 +0000

    Update command.py

--- commands/command.py
+++ commands/command.py
@@ -5,7 +5,7 @@ Commands describe the input the account can do to the game.
 
 """
 
-from evennia import create_object
+from evennia import create_object, utils
 from evennia.commands.command import Command as BaseCommand
 from world.skills import proficiency
 from server.conf.settings import RECOVERY_RATE, MEDITATION_COST, MINDSHIELD_GAIN, OPTIONAL_SKILLS
