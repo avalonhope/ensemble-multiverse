04e78b91f8daba070423ee0e2d4a7d620733ba07
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-21 11:47:22 +0000

    Update roleplaying.py

--- commands/roleplaying.py
+++ commands/roleplaying.py
@@ -36,37 +36,6 @@ class CmdDo(Command):
         self.caller.location.msg("%s %s" % self.caller.name, action)
         self.caller.location.events.append("%s %s" % self.caller.name, action)
         
-        
-class CmdSay(Command):
-    """
-    roleplay a converation
-
-    Usage:
-      +say <speech>
-
-    This emits a message to everyone in the location.
-    """
-    
-    key = "+say"
-    aliases = ["+speak"]
-    help_category = "roleplaying"
-
-    def func(self):
-        "This performs the actual command"
-        errmsg = "You must supply a valid string."
-        if not self.args:
-            self.caller.msg(errmsg)
-            return
-        try:
-            action = str(self.args.strip())
-        except ValueError:
-            self.caller.msg(errmsg)
-            return
-        
-        self.caller.msg("You say '%s'." % action)
-        self.caller.location.msg("%s says '%s'" % self.caller.name, action)
-        self.caller.location.events.append("%s said '%s'" % self.caller.name, action)
-        
 
 class CmdHistory(Command):
     """
