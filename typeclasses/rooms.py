eb61befd174e42b42ab0d8d72f5ece145982f11f
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-02-24 10:35:13 +0000

    Update rooms.py

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -5,8 +5,27 @@ Rooms are simple containers that has no location of their own.
 
 """
 
+from enum import Enum
 from evennia import DefaultRoom
 from commands.default_cmdsets import RoleplayingCmdset, TrainingCmdset
+
+
+class Location(Enum):
+    ROOM = 0
+    BUILDING = 1  # structure, cavern
+    AREA = 2  # street, village
+    DISTRICT = 3  # major village, town quarter
+    TOWN = 4  # space station, county
+    REGION = 5  # city, province, duchy
+    COUNTRY = 6  # major country or minor planet
+    PLANET = 7
+    SYSTEM = 8
+    SECTOR = 9
+    GALAXY = 10
+    CLUSTER = 11
+    SUPERCLUSTER = 12
+    UNIVERSE = 13 # inner worlds
+    MULTIVERSE = 14 
 
 
 class Room(DefaultRoom):
@@ -22,6 +41,7 @@ class Room(DefaultRoom):
     def at_object_creation(self):
         self.db.events = []
         self.cmdset.add(RoleplayingCmdset, permanent=True)
+        self.db.level = Location.ROOM
 
         
 class TrainingRoom(Room):
