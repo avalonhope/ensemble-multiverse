6a0a18208416dcec6c935b69aeafef3f60f626fb
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-03-01 09:32:03 +0000

    Update regions.py

--- typeclasses/regions.py
+++ typeclasses/regions.py
@@ -17,3 +17,17 @@ class Region(DefaultRoom):
 
     def at_object_creation(self):
         self.db.level = Location.REGION
+        
+       
+class Town(DefaultRoom):
+    """
+    Towns are like any Object, except their location is within a Region. They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+    See examples/object.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    def at_object_creation(self):
+        self.db.level = Location.TOWN
+        
