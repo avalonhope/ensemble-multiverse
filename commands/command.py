98731e9aa84fabcfa23ce77dffdeb61b3e28d9f3
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-03-07 14:22:38 +0000

    Update command.py

--- commands/command.py
+++ commands/command.py
@@ -432,11 +432,6 @@ class CmdStats(Command):
         self.caller.msg("Your stamina level is now %.2f." % proficiency(self.caller.db.stamina))
         for skillname in self.caller.db.skills.keys():
             self.caller.msg(f"Your {skillname} skill level is now {round(proficiency(self.caller.db.skills[skillname]), 2)}.")
-        
-        if self.caller.db.faction:
-            self.caller.msg("You are a member of " + self.caller.db.faction.name)
-        else:
-            self.caller.msg("You do not belong to any faction.")
             
         return
     
