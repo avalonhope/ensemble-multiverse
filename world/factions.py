8ffc1b2e9c7be215d0012e206680829ab1d0eaba
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-03-08 16:57:58 +0000

    Update factions.py

--- world/factions.py
+++ world/factions.py
@@ -1,15 +1,15 @@
 def subfaction(child, parent):
   """Check if one faction is a subfaction of another."""
   if child.db.superfaction is None:
-    return false
+    return False
   elif child.db.superfaction.name == parent.name:
-    return true
+    return True
   else:
     superfaction = child.db.superfaction
-    found = false
+    found = False
     while superfaction.db.superfaction is not None:
       if superfaction.db.superfaction.name == parent.name:
-        found = true
+        found = True
         break
       superfaction = superfaction.db.superfaction
     return found
