60f30443e32bb02928081e38f5551df9c5871a61
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-04-08 10:21:35 +0100

    Update default_cmdsets.py

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -46,6 +46,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(faction.CmdFactionJoin())
         self.add(faction.CmdFactionSpace())
         self.add(command.CmdTrainSkill())
+        self.add(command.CmdRace())
 
         
 
