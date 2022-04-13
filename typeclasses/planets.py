39e26e1407bc17c40603da6b8d90c6d799255368
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-04-13 17:28:45 +0100

    Update planets.py

--- typeclasses/planets.py
+++ typeclasses/planets.py
@@ -18,3 +18,31 @@ class Planet(DefaultRoom):
 
     def at_object_creation(self):
         self.db.level = Location.PLANET
+        
+class Moon(DefaultRoom):
+    """
+    Moons are like any Object, except their location 
+    is near a Planet within a Star System. They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+    See examples/object.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    def at_object_creation(self):
+        self.db.level = Location.COUNTRY
+        
+class Sun(DefaultRoom):
+    """
+    Suns are like any Object, except their location 
+    is at the centre of a Star System. They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+    See examples/object.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    def at_object_creation(self):
+        self.db.level = Location.PLANET
+        
+        
