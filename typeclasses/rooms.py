e72c6c43c87ec65a6f79549dedf455a3f9abde36
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-04-13 17:34:16 +0100

    Update rooms.py

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -17,14 +17,14 @@ class Location(IntEnum):
     DISTRICT = 3  # major village, town quarter, large ship
     TOWN = 4  # space station, county, shipyard, wilderness, floating city
     REGION = 5  # major city, province, duchy, atmospheric layer
-    COUNTRY = 6  # major country or minor planet, atmosphere of major planet
-    PLANET = 7  # inner worlds for most species
-    SYSTEM = 8
+    COUNTRY = 6  # major country or minor planet, atmosphere of major planet, moon
+    PLANET = 7  # planets, stars, inner worlds for most species
+    SYSTEM = 8 # single star system
     SECTOR = 9  # large group of star systems
-    GALAXY = 10
-    CLUSTER = 11
-    SUPERCLUSTER = 12  # or other large cosmic structure
-    UNIVERSE = 13 # inner worlds for advanced species
+    GALAXY = 10 # single galaxy, inner world for some species
+    CLUSTER = 11 # galaxy cluster or group
+    SUPERCLUSTER = 12  # any large cosmic structure
+    UNIVERSE = 13 # physical space-time continuum, inner worlds for advanced species
     MULTIVERSE = 14 # all of creation, visible and invisible, highest possible level
 
 
