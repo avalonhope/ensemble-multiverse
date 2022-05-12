4078671b1dba1ea7863d697f350b41fa96ca00a8
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-12 12:29:10 +0100

    Update quests.py

--- features/steps/quests.py
+++ features/steps/quests.py
@@ -4,15 +4,18 @@ from typeclasses import accounts
 
 @given("a new character has been created")
 def step_impl(context):
+"""Create a test character."""
     test_account = accounts.create("username", "password")
     test_account.create_character()
 
 
 @when("the quest command is used")
 def step_impl(context):
+"""Run the quest command."""
     pass
 
 
 @then("a list of quests is shown")
+"""Check that a list of quests is shown."""
 def setp_impl(context):
     pass
