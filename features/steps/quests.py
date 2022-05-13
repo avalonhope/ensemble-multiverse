512efa05e5ada8fa16ecf678189476a96c9442f9
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-13 08:58:07 +0100

    Update quests.py

--- features/steps/quests.py
+++ features/steps/quests.py
@@ -1,12 +1,9 @@
 from behave import *
-from typeclasses import accounts
 
 
 @given("a new character has been created")
 def step_impl(context):
     """Create a test character."""
-    test_account = accounts.create("username", "password")
-    test_account.create_character()
 
 
 @when("the quest command is used")
