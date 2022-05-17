458647df67b69935f06a61b49888273abc224b3e
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-17 09:49:58 +0100

    Update biospheres.py

--- features/steps/biospheres.py
+++ features/steps/biospheres.py
@@ -2,8 +2,11 @@ from behave import *
 
 from world import biospheres, planets
 
+@given("there is a planet")
+def step_impl(context):
+    context.planet = planets.Planet()
+
 @given("the planet has an atmosphere")
 def step_impl(context):
-  context.planet = planets.Planet()
   context.planet.atmosphere = planets.Atmosphere()
   
