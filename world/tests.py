d80eb0dd5d6ab91dd081e1e297c7b01af3e5d5ac
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-19 17:51:19 +0100

    Test proficiency

--- world/tests.py
+++ world/tests.py
@@ -1,1 +1,14 @@
+from evennia.utils.test_resources import EvenniaTest
+from world.skills import proficiency
 
+class TestSkills(EvenniaTest):
+    def test_proficiency(self):
+        expected = {
+          0: 1.0,
+          1000: 2.0,
+          8000: 3.0,
+          27000: 4.0,
+          64000: 5.0,
+        }
+        for value in expected.keys():
+          self.assertEqual(proficency(value), expected(value))
