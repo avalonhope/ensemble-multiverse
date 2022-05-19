c9e58854a31cbf74b8472c24d38c8c1801c85fad
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-19 14:15:29 +0100

    Update tests.py

--- world/tests.py
+++ world/tests.py
@@ -3,7 +3,13 @@ from worlds.skills import proficiency
 
 class TestSkills(EvenniaTest):
     def test_level1_proficiency(self):
-      self.assertEqual(proficiency(0), 1.0)
+        self.assertEqual(proficiency(0), 1.0)
       
     def test_level2_proficiency(self):
-      self.assertEqual(proficiency(1000), 2.0)
+        self.assertEqual(proficiency(1000), 2.0)
+    
+    def test_level3_proficiency(self):
+        self.assertEqual(proficiency(8000), 3.0)
+        
+    def test_negative_experience(self):
+        self.assertEqual(proficiency(-1), 1.0)
