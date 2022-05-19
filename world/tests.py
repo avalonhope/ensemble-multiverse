9930d36d24f2e2cd6ecbbe484057c68e91e37535
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-19 17:10:56 +0100

    Update tests.py

--- world/tests.py
+++ world/tests.py
@@ -1,6 +1,7 @@
 from evennia.utils.test_resources import EvenniaTest
 from worlds.skills import proficiency
 
+
 class TestSkills(EvenniaTest):
     def test_level1_proficiency(self):
         self.assertEqual(proficiency(0), 1.0)
