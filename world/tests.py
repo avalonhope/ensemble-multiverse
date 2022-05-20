9b5f509de76b2ecbf12d458d4df57c5fc6c3df2c
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-20 12:10:17 +0100

    Update tests.py

--- world/tests.py
+++ world/tests.py
@@ -2,7 +2,9 @@ from evennia.utils.test_resources import EvenniaTest
 from world.skills import proficiency
 
 class TestSkills(EvenniaTest):
+    """Test skills system."""
     def test_proficiency(self):
+        """"Test skill level calculation."""
         examples = {
             -1: 1.0,
             0: 1.0,
