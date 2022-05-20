8f426861d9ca55f05d03b5bbf9e422b6883cce5c
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-20 14:58:40 +0100

    Update settings.py

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -53,3 +53,6 @@ try:
     from server.conf.secret_settings import *
 except ImportError:
     print("secret_settings.py file not found or failed to import.")
+    SERVERNAME = "Starquest - Infinite Worlds"
+    WEBSERVER_ENABLED = False
+    
