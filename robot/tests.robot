c5422aafe9a988dbb1585fb925dabe7b0a9f1d00
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-21 09:04:42 +0100

    Empty Robot Test (#40)
    * Update tests.py
    
    * Update tests.py
    
    * Autofix issues in 1 file
    
    Resolved issues in world/tests.py via DeepSource Autofix
    
    * Update tests.py
    
    * Update requirements.txt
    
    * Create tests.robot
    
    * Autofix issues in 1 file
    
    Resolved issues in world/tests.py via DeepSource Autofix
    
    * Update tests.py
    
    * Update tests.py
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ robot/tests.robot
@@ -0,0 +1,1 @@
+Should Be True  True
--- world/tests.py
+++ world/tests.py
@@ -1,5 +1,33 @@
 from evennia.utils.test_resources import EvenniaTest
+from hypothesis import given
+from hypothesis.strategies import integers
 from world.skills import proficiency
+
+
+@given(integers())
+def test_proficiency_min_value(x):
+    """Test minimum value of result."""
+    if proficiency(x) < 1.0:
+        raise AssertionError
+
+
+@given(integers(), integers())
+def test_proficiency_strictly_increasing(x, y):
+    """Verify that result is strictly increasing."""
+    if 0 <= x < y and proficiency(x) >= proficiency(y):
+        raise AssertionError
+    if x > y >= 0 and proficiency(x) <= proficiency(y):
+        raise AssertionError
+    if x == y and proficiency(x) != proficiency(y):
+        raise AssertionError
+
+
+@given(integers())
+def test_inverse_proficiency(x):
+    """Test that the result is the correct value."""
+    skill_level = proficiency(x)
+    if x != ((skill_level - 1) * 10) ** 3:
+        raise AssertionError
 
 
 class TestSkills(EvenniaTest):
