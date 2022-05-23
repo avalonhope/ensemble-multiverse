2904f70d314ee0cb370626b81d91b830f2e653d3
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-23 14:55:33 +0100

    Implement faction tests

--- features/steps/factions.py
+++ features/steps/factions.py
@@ -1,40 +1,45 @@
 from behave import given, when, then
+from world import actions, characters, factions
+
 
 @given(u'we have a faction A')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given we have a faction A')
+    context.faction_A = factions.Faction()
 
 
 @given(u'we have a character C')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given we have a character C')
+    context.character_C = characters.Character()
 
 
 @given(u'C is a member of faction A')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given C is a member of faction A')
+    context.character_C.faction = context.faction_A
 
 
 @given(u'we have another faction B')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Given we have another faction B')
+    conext.faction_B = factions.Faction()
 
 
 @given(u'faction B is not a subfaction of A')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given faction B is not a subfaction of A')
+    context.faction_B.superfactions = None
 
 
 @given(u'faction A is not a subfaction of B')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given faction A is not a subfaction of B')
+    context.faction_B.superfactions = None
 
 
 @when(u'C asks to join faction B')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When C asks to join faction B')
+    context.result = actions.join_faction(context.character_A, context.faction_B)
 
 
 @then(u'C is not allowed to join faction B')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then C is not allowed to join faction B')
+    if not context.result:
+        raise AssertionError
+        
