5b71bf65caca5349153a4aac8c505741e7b88292
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-25 16:12:32 +0100

    Update planets.py

--- typeclasses/planets.py
+++ typeclasses/planets.py
@@ -4,7 +4,7 @@ Planets are located within a Star System and may contain Countries.
 """
 
 from evennia import DefaultRoom
-from rooms import Location
+from typeclasses.rooms import Location
 
 
 class Planet(DefaultRoom):
