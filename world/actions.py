cc19b5639fa0e1557db58e2874ce525c48785cff
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-25 15:04:32 +0100

    Update actions.py

--- world/actions.py
+++ world/actions.py
@@ -1,7 +1,15 @@
 import icontract
-from typeclasses.factions import Faction
-from typeclasses.characters import Character
+
+import world.characters
+import world.factions
 
 @icontract.requires(character is not None)
 @icontract.requires(faction is not None)
 def join_faction(character, faction):
+  """A character asks to join a faction."""
+  if character.faction() is faction:
+    return True
+  if character.faction() is None or faction.subfaction(character.faction()):
+    character.set_faction(faction)
+    return True
+  return False
