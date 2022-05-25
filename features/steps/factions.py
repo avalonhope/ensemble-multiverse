8ea3e5f282ba6db3c11acf0333eebb148c4b0e95
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-25 15:41:56 +0100

    Update factions.py

--- features/steps/factions.py
+++ features/steps/factions.py
@@ -1,45 +1,43 @@
-from behave import given, when, then
+import behave
 from world import actions, characters, factions
 
 
-@given(u'we have a faction A')
+@behave.given(u'we have a faction A')
 def step_impl(context):
     context.faction_A = factions.Faction()
 
 
-@given(u'we have a character C')
+@behave.given(u'we have a character C')
 def step_impl(context):
     context.character_C = characters.Character()
 
 
-@given(u'C is a member of faction A')
+@behave.given(u'C is a member of faction A')
 def step_impl(context):
     context.character_C.faction = context.faction_A
 
 
-@given(u'we have another faction B')
+@behave.given(u'we have another faction B')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given we have another faction B')
-    conext.faction_B = factions.Faction()
+    context.faction_B = factions.Faction()
 
 
-@given(u'faction B is not a subfaction of A')
-def step_impl(context):
-    context.faction_B.superfactions = None
-
-
-@given(u'faction A is not a subfaction of B')
+@behave.given(u'faction B is not a subfaction of A')
 def step_impl(context):
     context.faction_B.superfactions = None
 
 
-@when(u'C asks to join faction B')
+@behave.given(u'faction A is not a subfaction of B')
+def step_impl(context):
+    context.faction_B.superfactions = None
+
+
+@behave.when(u'C asks to join faction B')
 def step_impl(context):
     context.result = actions.join_faction(context.character_C, context.faction_B)
 
 
-@then(u'C is not allowed to join faction B')
+@behave.then(u'C is not allowed to join faction B')
 def step_impl(context):
-    if not context.result:
+    if context.result is True:
         raise AssertionError
-
