81b832c570f1b16ae0b2f801ce937b00f5a451eb
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-05-25 17:28:46 +0100

    Update default_cmdsets.py

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -15,7 +15,7 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 """
 
 from evennia import default_cmds
-from commands import command, faction
+from commands import command, faction  # type: ignore
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
