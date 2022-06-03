23c4b811d2fd9eca636e5204989b59399881cd5f
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-03 12:22:21 +0100

    Update quests.py

--- features/steps/quests.py
+++ features/steps/quests.py
@@ -1,19 +1,19 @@
 # type: ignore
-from behave import *
+from behave import given, when, then
 from typeclasses.characters import Character
 
 
 @given("a character exists")
-def step_impl(context):
+def step_create_character(context):
     """Create a test character."""
     context.character = Character()
 
 
-@when("the quest command is used")
-def step_impl(context):
-    """Run the quest command."""
+@when("the quests command is used")
+def step_call_quests_commands(context):
+    """Run the quests command."""
 
 
 @then("a list of available quests is shown")
-def setp_impl(context):
+def step_list_of_quests(context):
     """Check that a list of quests is shown."""
