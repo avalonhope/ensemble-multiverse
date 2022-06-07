c96e6b09f787f2a4e433c0c3e359900439163687
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-07 17:32:41 +0100

    Update test_logic.py

--- world/logic/test_logic.py
+++ world/logic/test_logic.py
@@ -1,6 +1,8 @@
 import unittest
-
+from world.logic.system import System
 
 class TestLogic(unittest.TestCase):
+    """Test Workflow and Business Logic."""
     def test_sanity_check(self):
-        self.assertTrue(True)
+        """Test System Integrity."""
+        self.assertTrue(system.active())
