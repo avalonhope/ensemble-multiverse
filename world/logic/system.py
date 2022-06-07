5f9a848717ac597cd23ecfc23d5596db92b0496c
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-07 19:55:03 +0100

    Update system.py

--- world/logic/system.py
+++ world/logic/system.py
@@ -1,6 +1,9 @@
+import icontract
+
 class System:
     """Workflow and Business Logic system for Starquest - Infinite Worlds."""
 
+    @icontract.ensures(lambda result: result is True)
     def active(self):
         """System integrity status."""
         return True
