583f4c5dd2fe0f8012debc890dab4d968b404e0f
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-08 09:52:37 +0100

    Update test_skills.py

--- world/logic/test_skills.py
+++ world/logic/test_skills.py
@@ -4,35 +4,31 @@ from hypothesis.strategies import integers
 from world.logic.skills import proficiency  # type: ignore
 
 
-@given(integers())
-def test_proficiency_min_value(x):
-    """Test minimum value of result."""
-    if proficiency(x) < 1.0:
-        raise AssertionError
-
-
-@given(integers(), integers())
-def test_proficiency_strictly_increasing(x, y):
-    """Verify that result is strictly increasing."""
-    if 0 <= x < y and proficiency(x) >= proficiency(y):
-        raise AssertionError
-    if x > y >= 0 and proficiency(x) <= proficiency(y):
-        raise AssertionError
-    if x == y and proficiency(x) != proficiency(y):
-        raise AssertionError
-
-
-@given(integers())
-def test_inverse_proficiency(x):
-    """Test that the result is the correct value."""
-    skill_level = proficiency(x)
-    if x != ((skill_level - 1) * 10) ** 3:
-        raise AssertionError
-
-
 class TestSkills(unittest.TestCase):
     """Test skills system."""
+    
+    @given(integers())
+    def test_proficiency_min_value(self, x):
+        """Test minimum value of result."""
+        self.assertGreaterEqual(proficiency(x), 1.0)
 
+    @given(integers(), integers())
+    def test_proficiency_strictly_increasing(self, x, y):
+        """Verify that result is strictly increasing."""
+        if 0 <= x < y:
+            self.assertGreater(proficiency(y), proficiency(x))
+        if x > y >= 0:
+            self.assertGreater(proficiency(x), proficiency(y))
+        if x == y:
+            self.assertEqual(proficiency(y), proficiency(x))
+
+
+    @given(integers())
+    def test_inverse_proficiency(self, x):
+        """Test that the result is the correct value."""
+        skill_level = proficiency(x)
+        self.assertEqual(x, ((skill_level - 1) * 10) ** 3)
+    
     def test_proficiency(self):
         """Test skill level calculation."""
         examples = {
@@ -46,3 +42,6 @@ class TestSkills(unittest.TestCase):
         }
         for experience, level in examples.items():
             self.assertEqual(proficiency(experience), level)
+            
+if __name__ == "__main__":
+    unittest.main()
