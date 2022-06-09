17a3e3771fc6f1ab1f4dea7ea5114cbd807a8234
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-09 09:45:18 +0100

    Update characters.py

--- world/logic/characters.py
+++ world/logic/characters.py
@@ -1,5 +1,18 @@
+from system import System as LogicSystem
+import icontract
+
 class LeadCharacter:
     """Leading character for a quest party."""
+    
+    def __init__(self):
+        """Lead character for a quest party."""
+        self.system = LogicSystem()
 
-    def command(self, command_name):
-        """Execute a command on this charcater or party."""
+    @icontract.require(lambda command_name: command_name is not None)
+    def command(self, command_name: str):
+        """Execute a command on this character or party."""
+        this_command = command_name.lower()
+        if this_command == "quests":
+            result = self.system.quests()
+        else result = None
+        return result
