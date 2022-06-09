8f790622e127cd3cfa41da50d6c85278f92076bd
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-09 15:15:45 +0100

    Update system.py

--- world/logic/system.py
+++ world/logic/system.py
@@ -1,12 +1,12 @@
 import icontract
-
+from world.quest_settings import QUESTS
 
 class System:
     """Workflow and Business Logic system for Starquest - Infinite Worlds."""
 
     def __init__(self):
         """The Beginning."""
-        self.list_of_quests = []
+        self.list_of_quests = QUESTS
 
     @icontract.ensure(lambda result: result is True)
     def active(self):
