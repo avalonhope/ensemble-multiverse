7e0640a5e875b3d3cd74af149b9c0a786f3f3aae
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-09 16:33:59 +0100

    Update and rename test_logic.py to test_system.py

--- world/logic/test_system.py
+++ world/logic/test_system.py
@@ -5,7 +5,12 @@ from world.logic.system import System as LogicSystem  # type: ignore
 class TestLogic(unittest.TestCase):
     """Test Workflow and Business Logic."""
 
-    def test_sanity_check(self):
+    def test_active(self):
         """Test System Integrity."""
         system = LogicSystem()
         self.assertTrue(system.active())
+        
+    def test_quests(self):
+        """Test List of Quests."""
+        
+        
