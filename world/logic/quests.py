827f49195176a12823ba83d09589b01f8c712f14
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-09 15:45:05 +0100

    Update quests.py

--- world/logic/quests.py
+++ world/logic/quests.py
@@ -1,9 +1,10 @@
 class Quest:
     """Quests are multi-user interactive stories."""
 
-    def __init__(self):
+    def __init__(self, name=""):
         """The beginning of the story."""
         self.participants = []
+        self.name = name
 
     def list_participants(self):
         """List of participants."""
@@ -12,3 +13,4 @@ class Quest:
     def add_participant(self, character):
         """Add character to the list of participants."""
         self.participants.append(character)
+        return f"{character.name} has joined the {self.name} quest."
