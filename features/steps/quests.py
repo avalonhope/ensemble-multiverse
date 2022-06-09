755f8156a856e8fcc363d98dd3502c09857d9412
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-09 08:12:25 +0100

    Update quests.py

--- features/steps/quests.py
+++ features/steps/quests.py
@@ -5,25 +5,37 @@ from world.logic.quests import Quest
 
 
 @given("a character exists")
-def step_create_character(context):
+def create_character(context):
     """Create a test character."""
     context.character = LeadCharacter()
 
 
 @given("a quest exists")
-def step_create_quest(context):
+def create_quest(context):
     """Create a quest."""
     context.quest = Quest()
 
 
 @when("the {command_name} command is used")
-def step_call_commands(context, command_name):
+def call_commands(context, command_name):
     """Run the command."""
     context.list_of_quests = context.character.command(command_name)
 
 
 @then("a list of available quests is shown")
-def step_check_list_of_quests(context):
+def check_list_of_quests(context):
     """Check that a list of quests is shown."""
     if context.list_of_quests is None:
         raise AssertionError
+ 
+
+@given('the quest has an existing partipant')
+def quest_already_has_participants(context):
+    context.quest.add_participant(LeadCharacter())
+
+
+@then('the character is added to the list of quest participants')
+def check_character_added_to_quest(context):
+    if context.character not in context.quest.participants():
+        raise AssertionError
+
