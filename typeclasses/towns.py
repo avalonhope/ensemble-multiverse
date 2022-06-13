4d3daa8f36f874b212aabde9c200c27d8084c5bd
avalonhope <96471977+avalonhope@users.noreply.github.com>
2022-06-13 11:10:25 +0100

    Create towns.py

---
+++ typeclasses/towns.py
@@ -0,0 +1,21 @@
+"""
+Towns
+"""
+
+from evennia import DefaultRoom
+from typing import Any
+from world.logic.locations import Location  # type: ignore
+
+
+class Town(DefaultRoom):
+    """
+    Towns are like any Object, except their location is within a Region.
+    They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+    See examples/object.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    def at_object_creation(self):
+        self.db.level = Location.TOWN
