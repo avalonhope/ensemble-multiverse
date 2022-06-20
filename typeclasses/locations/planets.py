65ceeb744d82fbe2e418dcfb67c22436fee2fee8
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-06-20 14:54:01 +0100

    Update and rename typeclasses/planets.py to typeclasses/locations/planets.py

---
+++ typeclasses/locations/planets.py
@@ -0,0 +1,11 @@
+""" Planets are located within a Star System and may contain Countries. """
+
+from typeclasses.locations import Location
+from world.logic.locations import LocationType
+
+
+class Planet(Location):
+    """Locations within a star system such as planets or moons."""
+
+    def at_object_creation(self):
+        self.db.locationType = LocationType.PLANET
