e22b416f9276b884c5f6bcba51c308ad512648ab
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-06-21 09:44:26 +0100

    Update and rename typeclasses/innerworld.py to typeclasses/locations/inner_worlds.py

---
+++ typeclasses/locations/inner_worlds.py
@@ -0,0 +1,10 @@
+"""Inner Worlds exist within the souls of characters."""
+from typeclasses.locations import Location
+from world.logic.location_types import LocationType
+
+
+class InnerWorld(Location):
+    """An inner world exists within the soul of a character."""
+
+    def at_object_creation(self):
+        self.db.locationType = LocationType.MULTIVERSE
