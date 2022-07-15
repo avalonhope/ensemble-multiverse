69b97add6c1cadd5176e95aa38c6fe7da159dd66
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-07-15 16:35:26 +0100

    Update towns.py

--- typeclasses/locations/towns.py
+++ typeclasses/locations/towns.py
@@ -1,13 +1,17 @@
 """Towns"""
 import icontract
 
+from evennia import DefaultExit
 from typeclasses.locations import Location
 from world.logic.locations import LocationType
 
 
 @icontract.invariant(lambda self: self.locationType == LocationType.TOWN)
 class Town(Location):
-    """Towns may contain Building and Vehicle objects."""
+    """Towns."""
 
     def at_object_creation(self):
         self.db.locationType = LocationType.TOWN
+        
+class Road(DefaultExit):
+    """Roads are the connectors for travel between Towns."""
