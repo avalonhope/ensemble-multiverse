17b2f426397cbf84b6291ae56b298602f4ddfbcd
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-08-09 20:34:59 +0100

    Update countries.py

--- typeclasses/locations/countries.py
+++ typeclasses/locations/countries.py
@@ -3,7 +3,7 @@ Country
 Countries are simple containers located within a Planet.
 """
 
-from typeclasses.locations.locations import Location
+from typeclasses.locations.locations import Location  # type: ignore
 from world.logic.location_types import LocationType  # type: ignore
 
 
