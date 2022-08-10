56a491b90c6ed0a94a9c35966881b67fb9c6da1a
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-08-10 11:03:04 +0100

    Update towns.py

--- tests/features/steps/towns.py
+++ tests/features/steps/towns.py
@@ -2,43 +2,36 @@ from behave import given, when, then
 
 @then(u'a list of zones within the town is shown')
 def zones(context):
-    context.zones = context.town.zones()
+    
 
 @when(u'the roads command is used')
 def roads(context):
-    context.roads = context.town.roads()
 
 
 @then(u'a list of roads out of town is shown')
 def show_roads(context):
-    if context.roads is None:
-        raise AssertionError("Roads not found.")
+
 
 @given(u'a charcater is located within a town')
-def intown(context):
-    context.character.town = context.town
+def in_town(context):
 
 
 @given(u'the charcater is onboard a vehicle')
-def invehicle(context):
+def in_vehicle(context):
     
 
-
 @given(u'the charcater has control of the vehicle')
-def driving(context):
-    raise NotImplementedError(u'STEP: Given the charcater has control of the vehicle')
+def driving_vehice(context):
 
 
 @when(u'the travel command is used')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When the travel command is used')
+def travel(context):
 
 
 @when(u'a road is specified')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When a road is specified')
+def choose_road(context):
 
 
 @then(u'the vehicle enters that road')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the vehicle enters that road')
+def check_vehicle_on_road(context):
+    
