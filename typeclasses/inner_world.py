a1a7b29981dccc3c07aa11e40562eb3f025abe48
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-11-17 13:39:45 +0000

    Create inner_world.py

---
+++ typeclasses/inner_world.py
@@ -0,0 +1,13 @@
+"""
+Inner Worlds
+Inner Worlds exist within characters that are Plural Systems.
+"""
+from evennia import DefaultRoom
+from typeclasses.rooms import Location  # type: ignore
+
+
+class Home(DefaultRoom):
+    """Home location for arriving at an inner world."""
+
+    def at_object_creation(self):
+        self.db.level = Location.UNIVERSE
