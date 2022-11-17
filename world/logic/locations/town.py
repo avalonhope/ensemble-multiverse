4ed0083ea14df79f77ed3bbf44ba4ce1c26963da
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-11-17 17:12:28 +0000

    Update town.py

--- world/logic/locations/town.py
+++ world/logic/locations/town.py
@@ -4,3 +4,9 @@ class Road:
 
 class Town:
     """A settlement within a region."""
+    
+    def search_roads(self):
+        """List all the roads connected to this town."""
+        
+    def search_zones(self):
+        """List all the zones within this town."""
