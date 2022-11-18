4865a9f647cbc2cdf5fd6cb286c659feffc27c21
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-11-18 16:43:27 +0000

    Update mobile.py

--- world/logic/mobile.py
+++ world/logic/mobile.py
@@ -1,5 +1,8 @@
+from world.logic.locations.town import Road
+
 class Vehicle:
     """An entity that may move between locations and may contain passengers and cargo."""
-
+    self.road_location = Nonw
+    
     def set_driver(self, driver):
         """Assign a character as driver of this vehicle."""
