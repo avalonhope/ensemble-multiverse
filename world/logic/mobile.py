993ac080e53c4cb2b9d76f9bbb9cce2a6d138da4
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-11-18 16:49:36 +0000

    Update mobile.py

--- world/logic/mobile.py
+++ world/logic/mobile.py
@@ -1,10 +1,13 @@
-from world.logic.locations.town import Road
-
-
 class Vehicle:
     """An entity that may move between locations and may contain passengers and cargo."""
 
-    self.road_location = Nonw
+    self.road_location = None
+    self.driver = None
 
     def set_driver(self, driver):
         """Assign a character as driver of this vehicle."""
+        self.driver = driver
+        
+    def select_road(self, road):
+        """Move the vehicle onto a road."""
+        self.road_location = road
