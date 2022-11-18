d0dd906b64e55a614ff836a52923cc1b04ba4ae8
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-11-18 13:56:36 +0000

    Update town.py

--- world/logic/locations/town.py
+++ world/logic/locations/town.py
@@ -4,9 +4,17 @@ class Road:
 
 class Town:
     """A settlement within a region."""
+    
+    def __init__(self, first_zone):
+        """Create a new town."""
+        self.zones = [first_zone]
 
     def search_roads(self):
         """List all the roads connected to this town."""
 
     def search_zones(self):
         """List all the zones within this town."""
+        return self.zones
+    
+class Zone:
+    """A zone within a town."""
