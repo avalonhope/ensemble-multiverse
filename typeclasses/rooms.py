df5813cc682c6b42874b5fe45cde6e9000efae69
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2022-11-21 12:14:06 +0000

    Update rooms.py

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -10,7 +10,8 @@ from evennia import DefaultRoom
 
 
 class Location(IntEnum):
-    """Each level is contained with the next level up, for example a room is inside a building."""
+    """Each level is contained with the next level up, 
+    for example a room is inside a building."""
 
     ROOM = 0  # cannot have any sub-rooms, lowest possible level
     BUILDING = 1  # structure, cavern, vehicle, shuttle
