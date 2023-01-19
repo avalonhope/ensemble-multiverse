fe0e2cadf2cb380a82505edaebf62dc28f01e623
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2023-01-19 14:33:48 +0000

    Create scenes.py

---
+++ typeclasses/scenes.py
@@ -0,0 +1,14 @@
+"""
+Narrative Scene
+Scenes are the building blocks of interactive narratives.
+"""
+
+from evennia import DefaultRoom
+
+
+class Scene(DefaultRoom):
+    """
+    Scenes are the building blocks of interactive stories.
+    They are like rooms but they also contain events.
+    """
+ 
