8f6b8a87dd6d517f25d38c53cd4e8c25e3cd4944
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2023-06-02 13:43:35 +0100

    Update timelines.py

--- typeclasses/timelines.py
+++ typeclasses/timelines.py
@@ -1,5 +1,9 @@
-from evennia import DefaultRoom
+from evennia import DefaultScript
 
-class Timeline(DefaultRoom):
+class Timeline(DefaultScript):
   
+  def __init__(self):
+        """Timelines are persistent."""
+        super().__init__()
+        self.persistent = True
   
