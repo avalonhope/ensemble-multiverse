06a57cbdb424a22b2e997ddfec6d7a043536b04a
Avalon Hope <96471977+avalonhope@users.noreply.github.com>
2023-06-14 09:26:15 +0100

    Update test_faction_commands.py

--- commands/tests/test_faction_commands.py
+++ commands/tests/test_faction_commands.py
@@ -1,10 +1,10 @@
-from evennia.utils.test_resources import BaseEvenniaCommandTest
+# type: ignore
 
+from evennia.utils.test_resources import EvenniaCommandTest
 from typeclasses.characters import Character  # type: ignore
 from typeclasses.rooms import Room  # type: ignore
 
-
-class TestFactionClaim(BaseEvenniaCommandTest):
+class TestFactionClaim(EvenniaCommandTest):
     """Test faction claims and disputes."""
 
     character_typeclass = Character
